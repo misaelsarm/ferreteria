f1444cc597cb3a44c194030ed6855e8f4f603d96
misaelsarm <misaelsarmiento97@gmail.com>
2020-04-04 15:18:42 -0600

    Se agregaron navbar y rutas. Diseño de navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
+import { AjustesComponent } from './components/ajustes/ajustes.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: HomeComponent },
+  { path: 'buscar', component: BuscarComponent },
+  { path: 'pedidos', component: PedidosComponent },
+  { path: 'ajustes', component: AjustesComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { AjustesComponent } from './components/ajustes/ajustes.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PedidosComponent,
+    AjustesComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
