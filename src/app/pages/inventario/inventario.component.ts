cfcb147894d4c1824fe815b48f89c617cf52265f
Jose Miguel <mike_trko@hotmail.com>
2020-05-12 02:17:56 -0500

    formulario inventario

--- src/app/pages/inventario/inventario.component.ts
+++ src/app/pages/inventario/inventario.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {NgModule} from "@angular/core";
+import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { Item } from 'src/app/models/item.model';
+import { FerreteriaService } from 'src/app/services/ferreteria.service';
 
 
 @Component({
@@ -15,15 +16,17 @@ import { Item } from 'src/app/models/item.model';
 export class InventarioComponent implements OnInit {
 
   item = new Item();
-  
-  constructor() { }
+
+  constructor(private ferreteria: FerreteriaService) { }
 
   ngOnInit(): void {
   }
 
-OnclickSubmit(formData){
-  alert("Item Name: " + formData.Name);
-}
+  OnclickSubmit(formData) {
+    this.ferreteria.agregarItem(this.item)
+    console.log(formData);
+  }
+
 
 }
 
--- src/app/services/ferreteria.service.ts
+++ src/app/services/ferreteria.service.ts
@@ -12,17 +12,17 @@ export class FerreteriaService {
   items: Item[] = [];
 
   constructor(private afs: AngularFirestore) {
-    
+
     // this.agregarItem();
   }
 
-  agregarItem(this.item) {
-    const productCollection = this.afs.collection('Products');
-    productCollection.add({
-      Name: this.item.nombre, 
-      Description: this.item.descripcion,
+  agregarItem(item: Item) {
+    this.afs.collection('Products').doc('id').set({
+      nombre: this.item.nombre,
+      descripcion: this.item.descripcion
     });
-    
+
+
     // const item2 = new item(57392937589, 'martillo', 139, 'assets/img/martillo.png', 'forjada de acero al cromo vanadio dos veces más resistente al desgaste.', 'truper', true, 25);
     // const item3 = new item(57392937578, 'martillo', 139, 'assets/img/martillo.png', 'forjada de acero al cromo vanadio dos veces más resistente al desgaste.', 'truper', true, 25);
     // const item4 = new item(57392993758, 'martillo', 139, 'assets/img/martillo.png', 'forjada de acero al cromo vanadio dos veces más resistente al desgaste.', 'truper', true, 25);
@@ -33,7 +33,7 @@ export class FerreteriaService {
     /* const item2 = new Item(2, 'Desarmador', 29, 'assets/img/desarmador.PNG');
     const item3 = new Item(3, 'Brocha', 40, 'assets/img/brocha.PNG');
     const item4 = new Item(4, 'Pinzas', 99, 'assets/img/pinzas.PNG'); */
-   
+
     console.log(this.items);
   }
 
